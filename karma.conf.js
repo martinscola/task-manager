81833e390132455a74e0a3a1a66aac3c39e79f41
Martín Scola <scola.martin@gmail.com>
2021-02-16 13:54:48 -0300

    fixed moronicness

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/tareas'),
+      dir: require('path').join(__dirname, './coverage/tasks'),
       subdir: '.',
       reporters: [
         { type: 'html' },
